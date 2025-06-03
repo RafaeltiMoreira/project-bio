879f7fe4431f4ac1204ad33172685c5555f144ee
Rafael Moreira <rafaeltimoreirass@gmail.com>
2025-06-03 02:47:22 -0300

    feat: add implement config app

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,44 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import imageCompression from "browser-image-compression";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function sanitizeLink(link?: string) {
+  if (!link) return "";
+
+  return link
+    .replace(/\s/g, "")
+    .replace(/[!@#$%^&*()_+\-=\[\]{};':"\\|,çˆ.<>\/?]+/, "")
+    .toLocaleLowerCase();
+}
+
+export async function compressFiles(files: File[]) {
+  const compressPromises = files.map(async (file) => {
+    try {
+      return await compressImage(file)
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  });
+
+  return (await Promise.all(compressPromises)).filter((file) => file !== null);
+};
+
+export const compressImage = (file: File): Promise<File> => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      maxSizeMB: 0.2, // 200 KB
+      maxWidthOrHeight: 900, // 900px
+      useWebWorker: true,
+      fileType: "image/png",
+    };
+
+    imageCompression(file, options).then((compressedFile) => {
+      resolve(compressedFile);
+    });
+  });
+};
